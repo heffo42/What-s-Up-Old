9c0cf2f251355bdda29674e118eb0d27bc61fafb
David Heffernan <davidheffernan@davids-macbook-pro-3.local>
2015-12-29 00:24:27 -0500

    Initial Commit

---
+++ What's Up/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  What's Up
+//
+//  Created by David Heffernan on 12/29/15.
+//  Copyright © 2015 David Heffernan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
